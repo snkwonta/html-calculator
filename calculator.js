6330ad430d3517d5e8f22a20ed65a7bb14ac128d
Sody Nkwonta <sodienye.nkwonta@ucalgary.ca>
2020-02-10 10:35:21 -0700

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,3 +1,16 @@
 function show(result){
     form.textview.value=form.textview.value+result; 
 }  
+
+function backspace(){
+    form.textview.value=form.textview.value.substring(0, form.textview.value.length-1);
+}
+
+try {
+    eval('var valid_statement = 7; \n invalid_statement())))');
+} catch (error) {
+    var err = new Error();
+    err.message = 'Error. Cannot evaluate ' + err.message;
+    throw err;
+    
+}
